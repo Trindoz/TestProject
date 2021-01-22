d1a0f6c2522c78ea368057611b48ac6afc686cf8
Иван Долматов <ivandolmatov@list.ru>
2021-01-22 17:48:11 +0400

    c

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -9,10 +9,7 @@ namespace TestProject
     class Program
     {
         static void Main(string[] args)
-        {
-            Triangle triangle= new Triangle(3, 4, 6);
-            Console.WriteLine(triangle.IsSquared());
-            Console.ReadLine();
+        {         
         }
     }
 }
