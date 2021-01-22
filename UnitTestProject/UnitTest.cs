75f8df0c59cef9520941567dad435fff50e93082
Иван Долматов <ivandolmatov@list.ru>
2021-01-22 17:41:54 +0400

    commit

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestProject;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Correct_Round_Area()
+        {
+            double expected=153.86;         
+            Round round = new Round(7);
+            double area = round.GetArea();   
+            Assert.AreEqual(expected, area, 0.01, "Incorrect");
+          
+        }
+        [TestMethod]
+        public void Correct_TriangleArea()
+        {
+            double expected = 13.41;
+            Triangle triangle = new Triangle(7, 4, 7);
+            double area = triangle.GetArea();
+            Assert.AreEqual(expected, area, 0.01, "Incorrect");
+        }
+        [TestMethod]
+        public void Is_Triangle_Squared()
+        {
+            bool expected = true;
+            Triangle triangle = new Triangle(5, 4, 3);
+            bool result = triangle.IsSquared();
+            Assert.AreEqual(expected, result, "Incorrect");
+        }
+    }
+}
